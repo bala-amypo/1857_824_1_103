a9499bfb0a14e47fcc304ed5ab5c1f3b1d16607a
bala-amypo <bala@amypo.in>
2025-12-18 10:17:46 +0000

    User commit

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -12,7 +12,7 @@ public class Employee{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-    private String fullname;
+    private String fullName;
     private String email;
     private String role="STAFF";
     privare Long maxWeeklyHours;
@@ -21,7 +21,23 @@ public class Employee{
     public Long getId(){
         return id;
     }
-    public void setId(Long id){}
+    public void setId(Long id){
         this.id=id;
     }
+
+    public String getFullName(){
+        return fullName;
+    }
+    public void setFullName(String fullName){
+        this.fullName=fullName;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+
+    public 
 }
