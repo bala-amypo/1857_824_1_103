d5e521c9b8abebde83004beb9be25fd725fa55c9
bala-amypo <bala@amypo.in>
2025-12-18 10:07:46 +0000

    User commit

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -12,14 +12,9 @@ public class Employee{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private long id;
-
-    @Column(nullable=false)
     private String fullname;
-    @Column(nullable=false, unique=true)
     private String email;
-
-    @Column(nullable=false)
-    private String role;
-
+    private String role="STAFF";
+    privare 
     
 }
