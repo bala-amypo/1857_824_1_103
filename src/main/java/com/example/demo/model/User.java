edb3c06700d71c5bb46fe464761b0891ea7f1a92
bala-amypo <bala@amypo.in>
2025-12-18 04:47:58 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -12,52 +12,57 @@ public class User{
     @GeneratedVlaue(strategy=GenerationType.IDENTITY)
     private int id;
 
-    @X
+    @Column(nullable=false)
     private String name;
-   private String email;
-   private String password;
-   private String role;
 
-   public User(){
+    @Column(nullable=false, unique=true)
+    private String email;
 
-   }
+    @Column(nullable=false)
+    private String password;
 
-   public User(int id, String name, String email, String password, String role){
-    this.id=id;
-    this.name=name;
-    this.email=email;
-    this.password=password;
-    this.role=role;
-   }
-   public int getId(){
-    return id;
-   }
-    public void setId(){
-        this.id=id;
+    private String role;
+
+    public User(){
+
     }
-   public String getName(){
-    return name;
-   }
-   public void setName(String name){
-    this.name=name;
-   }
-   public String getEmail(){
-    return email;
-   }
-   public void setEmail(String email){
-    this.email=email;
-   }
-   public String getPassword(){
-    return password;
-   }
-   public void setPassword(String password){
-    this.password=password;
-   }
-   public String getRole(){
-    return role;
-   }
-   public void setRole(String role){
-    this.role=role;
-   }
+
+    public User(int id, String name, String email, String password, String role){
+        this.id=id;
+        this.name=name;
+        this.email=email;
+        this.password=password;
+        this.role=role;
+    }
+    public int getId(){
+        return id;
+    }
+        public void setId(){
+            this.id=id;
+        }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+    public String getRole(){
+        return role;
+    }
+    public void setRole(String role){
+        this.role=role;
+    }
 
 }
