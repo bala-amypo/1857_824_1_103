5478250da82d5481c68a133a598c5c8af40396b1
bala-amypo <bala@amypo.in>
2025-12-18 04:52:58 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -34,33 +34,44 @@ public class User{
         this.password=password;
         this.role=role;
     }
+
+
     public int getId(){
         return id;
     }
-        public void setId(){
-            this.id=id;
-        }
+
+    public void setId(){
+        this.id=id;
+    }
+
     public String getName(){
         return name;
     }
+
     public void setName(String name){
         this.name=name;
     }
+
     public String getEmail(){
         return email;
     }
+
     public void setEmail(String email){
         this.email=email;
     }
+
     public String getPassword(){
         return password;
     }
+
     public void setPassword(String password){
         this.password=password;
     }
+
     public String getRole(){
         return role;
     }
+    
     public void setRole(String role){
         this.role=role;
     }
