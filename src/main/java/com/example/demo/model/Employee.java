bc924e8d313f1c62d5f44df8ea701ce85b095787
bala-amypo <bala@amypo.in>
2025-12-19 06:33:19 +0000

    User commit

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
-import jakarta.persistence.LocalDateTime;
+import jakarta.time.LocalDateTime;
 
 @Entity
 
