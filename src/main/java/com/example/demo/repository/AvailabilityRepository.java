359b0ca6189ff7bb2c5144146c070f8e1b460204
bala-amypo <bala@amypo.in>
2025-12-23 05:13:14 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/AvailabilityRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Availability;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AvailabilityRepository
+        extends JpaRepository<Availability, Long> {
+}
