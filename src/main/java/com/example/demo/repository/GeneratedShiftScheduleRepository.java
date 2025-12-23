ccc2097958175c676c696a9d78a2305510642659
bala-amypo <bala@amypo.in>
2025-12-23 05:31:14 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/GeneratedShiftScheduleRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.GeneratedShiftSchedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GeneratedShiftScheduleRepository
+        extends JpaRepository<GeneratedShiftSchedule, Long> {
+}
