b85d6fc3fe403c7ed41b158e797d687d14775c3f
bala-amypo <bala@amypo.in>
2025-12-23 04:37:14 +0000

    User commit

--- src/main/java/com/example/demo/repository/AvailabilityRepository.java
+++ src/main/java/com/example/demo/repository/AvailabilityRepository.java
@@ -1,8 +1,8 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.Entityclass.User;
+import com.example.demo.Entityclass.EmployeeAvailability;
 
-public interface UserRepository extends JpaRepository<User,Long>{
+public interface AvailabilityRepository extends JpaRepository<User,Long>{
     
 }
--- src/main/java/com/example/demo/repository/DepartmentRepository.java
+++ src/main/java/com/example/demo/repository/DepartmentRepository.java
@@ -1,8 +1,8 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.Entityclass.User;
+import com.example.demo.Entityclass.Department;
 
-public interface UserRepository extends JpaRepository<User,Long>{
+public interface DepartmentRepository extends JpaRepository<Department,Long>{
     
 }
