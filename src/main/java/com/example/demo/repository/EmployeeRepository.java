a986bee3750769cc1fc6b7cf38a3523ded55cb96
bala-amypo <bala@amypo.in>
2025-12-23 04:52:14 +0000

    User commit

--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-    Optional<Employee> findByEmail(String email);
-
-    boolean existsByEmail(String email);
-}
--- src/main/java/com/example/demo/repository/GeneratedShiftScheduleRepository.java
+++
@@ -1,5 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GeneratedShiftScheduleRepository
-        extends JpaRepository<GeneratedShiftSchedule, Long> {
-}
